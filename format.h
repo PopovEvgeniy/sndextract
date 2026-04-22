bccf14a809d4a25584dd557653eee4ed2e779688
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2026-04-22 22:11:10 +0700

    The small changes

--- format.h
+++ format.h
@@ -2,15 +2,15 @@ typedef struct
 {
  char signature[12];
  unsigned char version[4];
- unsigned long int amount:32;
- unsigned long int first_offset:32;
+ unsigned int amount;
+ unsigned int first_offset;
  char comment[488];
 } head;
 
 typedef struct
 {
- unsigned long int next_offset:32;
- unsigned long int legth:32;
- unsigned long int group:32;
- unsigned long int sample:32;
+ unsigned int next_offset;
+ unsigned int legth;
+ unsigned int group;
+ unsigned int sample;
 } subhead;
